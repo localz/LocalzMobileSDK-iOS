8bb44e5a224d67adfb6b3f06a0f79f53157c2fbb
Melvin Artemas <melvin.artemas@localz.co>
2021-08-20 20:04:16 +1000

    LocalzMobileSDK 1.0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# LocalzMobileSDK-iOS (Preview release)
+
---
+++ Sources/LocalzMobileSDK.xcframework/ios-arm64/LocalzMobileSDK.framework/Headers/LocalzMobileSDK.h
@@ -0,0 +1,18 @@
+//
+//  LocalzMobileSDK.h
+//  LocalzMobileSDK
+//
+//  Created by Melvin Artemas on 20/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalzMobileSDK.
+FOUNDATION_EXPORT double LocalzMobileSDKVersionNumber;
+
+//! Project version string for LocalzMobileSDK.
+FOUNDATION_EXPORT const unsigned char LocalzMobileSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalzMobileSDK/PublicHeader.h>
+
+
